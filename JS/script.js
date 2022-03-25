2df556c63e862afbe5ea697ec71983b7780de546
mar-lima <marcelo.llima135@gmail.com>
2022-03-25 19:09:19 -0300

    first commit

---
+++ JS/script.js
@@ -0,0 +1,35 @@
+const doc =(el) => document.querySelector(el);
+const docAll =(el) => document.querySelectorAll(el);
+
+let body = doc('body')
+let target = docAll('.banner')
+
+function anime() {
+    const windowTop = window.scrollY + ((window.innerHeight * 3) / 4);
+    target.forEach(function(element) {
+        if((windowTop) > element.offsetTop) {
+            element.classList.add('animar')
+        } else {
+            element.classList.remove('animar')
+        }
+    })
+}
+
+if(target.length) {
+window.addEventListener('scroll', debounce(function(){
+    anime()
+    console.log('contador')
+}, 200));
+}
+
+function debounce(fn, delay) {
+    let timer = null
+    return function() {
+        let context = this
+        let args = arguments
+        clearTimeout(timer)
+        timer = setTimeout(function() {
+            fn.apply(context, args)
+        }, delay)
+    }
+}
